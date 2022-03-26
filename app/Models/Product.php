b5640ef54e087b35aa4a9d72b67acecbf4af778e
Joni <minjonlu@gmail.com>
2022-03-26 10:03:59 +0200

    Create ProductFactory and define it for Product - model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return ProductFactory::new();
+    }
 }
