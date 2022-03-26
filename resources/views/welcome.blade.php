008b94576ad95bd45fd1d157d87e2b61c81e80c3
Joni <minjonlu@gmail.com>
2022-03-26 17:40:40 +0200

    Link from home page to products listing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
                     </div>
                 </div>
             </div>
-            <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            <button onclick="location.href='{{ route('public.products.index') }}';" class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Browse Products
             </button>
         </div>
