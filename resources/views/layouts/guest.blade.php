93af1b1918b38abe780e8ffa48f84fd527af5080
Joni <minjonlu@gmail.com>
2022-03-26 11:37:46 +0200

    Add home/landing page for project

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,8 +16,23 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
+    <body class="bg-gray-100">
+        <header class="text-gray-600 body-font bg-white">
+            <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                    <x-icon-shopping-cart class="h-8 w-8 text-indigo-600" />
+                    <span class="ml-3 text-xl">WebShop</span>
+                </a>
+                <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                    <a class="mr-5 hover:text-gray-900">Products</a>
+                </nav>
+                <a href="/login" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+                    Login
+                    <x-icon-key class="h-6 w-6 text-gray-800" />    
+                </a>
+            </div>
+        </header>
+        <div class="font-sans text-gray-900 antialiased bg-gray-100">
             {{ $slot }}
         </div>
     </body>
