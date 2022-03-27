74d0916a2fddd63ac685e0acd1c66158571edfa5
Joni <minjonlu@gmail.com>
2022-03-27 11:14:46 +0300

    SignIn helper to help testing later

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,11 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\Traits\CanSignIn;
 
 abstract class TestCase extends BaseTestCase
 {
+    use CanSignIn;
     use CreatesApplication;
     use RefreshDatabase;
 }
